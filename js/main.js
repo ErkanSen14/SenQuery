f52a908d34f7b53ff37ea67e6d05bf00a0b91ba1
Erkan Sen <erkansen14@hotmail.com>
2016-09-21 01:43:39 -0500

    added nodes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-var senQuery = function() {
+;(function(){
+  'use strict';
+  var senQuery = function(selector) {
+    if(!(this.instanceof senQuery)) {
+      return new senQuery(selector);
+    }
 
+    if (typeof selector !== 'string') {
+      return this;
+    }
+    
+    var nodes = document.querySelectorAll(selector);
+    nodes.forEach(function(value, index){
+      this[index] = value;
+    });
+    this.length = nodes.length;
+
+    return this;
 }
 
 window.senQuery = senQuery;
 window.$ = senQuery;
+})()
