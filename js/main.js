f1d22e8fc0fcdde7440c1a67a4afb35492274b5c
Erkan Sen <erkansen14@hotmail.com>
2016-09-23 01:35:31 -0500

    fixed parenthesis

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 ;(function(){
   'use strict';
   var senQuery = function(selector) {
-    if(!(this.instanceof senQuery)) {
+    if(!(this.instanceof (senQuery))) {
       return new senQuery(selector);
     }
 
     if (typeof selector !== 'string') {
       return this;
     }
-    
+
     var nodes = document.querySelectorAll(selector);
     nodes.forEach(function(value, index){
       this[index] = value;
-    });
+    }, this);
     this.length = nodes.length;
 
     return this;
@@ -20,4 +20,5 @@
 
 window.senQuery = senQuery;
 window.$ = senQuery;
+console.log('loaded')
 })()
